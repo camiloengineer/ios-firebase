1c8fd4df8463b6c2ae60e928f1e5b8d08f47624b
Camilo González <cgonzalezm1234@gmail.com>
2022-06-24 06:41:20 -0400

    Initial Commit

---
+++ FirebaseTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTutorial
+//
+//  Created by Camilo Gonzalez on 24-06-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
